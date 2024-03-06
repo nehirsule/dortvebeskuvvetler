271b106b7388106860f6969099c77ccd5066f73d
Şule Nehir ARAÇ <147002677+nehirsule@users.noreply.github.com>
2024-03-06 13:13:02 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //n kullanıcının gireceği değer
+
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sınır sayısını giriniz: ");
+        n = input.nextInt();
+
+        //4'ün üslerini i diye tanımladım
+
+            for (int i = 1; i <= n; i*=4) {
+                System.out.println(i);
+            }
+        //5'in üslerini de m diye tanımladım.
+
+            for (int m = 1; m <= n; m*=5) {
+                System.out.println(m);
+            }
+
+    }
 }
